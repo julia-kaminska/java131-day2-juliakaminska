e20df43bb8bbdac7e12892ec087fed283f086de5
julia-kaminska <kaminska.julia@interia.pl>
2023-01-21 13:33:51 +0100

    Exercise 8 validator full version

--- src/main/java/exerciseEight/Main.java
+++ src/main/java/exerciseEight/Main.java
@@ -2,11 +2,17 @@ package exerciseEight;
 
 public class Main {
     public static void main(String[] args) {
-        Parcel parcel1 = new Parcel(10, 100, 30,50,true);
-        Parcel parcel2 = new Parcel(100, 100, 301,50,true);
+        Parcel parcel1 = new Parcel(10, 100, 10,40.4f,true);
+        Parcel parcel2 = new Parcel(10, 100, 10,12f,true);
+        Parcel parcel3 = new Parcel(10, 100, 10,30.5f,false);
+        Parcel parcel4 = new Parcel(100, 100, 101,17,false);
 
         Validator validator = new Validator();
-        validator.isDimensionViable(parcel1);
-        validator.isDimensionViable(parcel2);
+
+
+        validator.validate(parcel1);
+        validator.validate(parcel2);
+        validator.validate(parcel3);
+        validator.validate(parcel4);
     }
 }
--- src/main/java/exerciseEight/Validator.java
+++ src/main/java/exerciseEight/Validator.java
@@ -2,12 +2,25 @@ package exerciseEight;
 
 public class Validator {
 
-    public void isDimensionViable(Parcel parcel){
+    private void isDimensionViable(Parcel parcel){
         if ((parcel.getxLength() + parcel.getyLength() + parcel.getzLength()) <=300){
-            System.out.println("Paczka " + parcel.toString() + " spełnia wymogi");
+            System.out.println("Paczka " + parcel + " spełnia wymogi");
         }else {
-            System.out.println("Paczka " + parcel.toString() + " nie spełnia wymogów");
+            System.out.println("Paczka " + parcel + " nie spełnia wymogów");
         }
+    }
+    private void isWeightAlright(Parcel parcel){
+        if (parcel.getWeight() <= 30 && !parcel.isExpress()){
+            System.out.println("Paczka " + parcel + "jest dobrej wagi i nie jest ekspresowa");
+        } else if (parcel.getWeight() <= 15 && parcel.isExpress()) {
+            System.out.println("Paczka " + parcel + "jest dobrej wagi i jest ekspresowa");
+        }else {
+            System.out.println("Paczka " + parcel + "jest złej wagi");
+        }
+    }
+    public void validate(Parcel parcel){
+        isDimensionViable(parcel);
+        isWeightAlright(parcel);
     }
 
 }
